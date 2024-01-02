8c7d58029f0c8dd0971a926977ccab48562c6868
Kalle Sipila <kalle.t.sipila@gmail.com>
2024-01-02 23:13:03 +0800

    Connect firebase db

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-832ca-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
+
 
 addButtonEl.addEventListener("click", function () {
-    let inputValue = inputFieldEl.value
-    console.log(inputValue)
-
+    let inputValue = inputFieldEl.value;
+    clearInputFieldEl();
+    appendItemToShoppingListEl(inputValue);
+    push(shoppingListInDB, inputValue);
 })
 
+
+function clearInputFieldEl() {
+    inputFieldEl.value = "";
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+}
