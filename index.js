c9fb741f11e3036e620f35a90e3702fff2c203e8
Kalle Sipilä <kalle.t.sipila@gmail.com>
2024-01-06 17:05:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebas
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://playground-832ca-default-rtdb.asia-southeast1.firebasedatabase.app/"
+    databaseURL: ""
 }
 
 
